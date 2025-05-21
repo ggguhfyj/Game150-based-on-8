2b650fd55fec8ad0e359b2986323bf536fc8816c
SEOK WON JUNG <56464531+Seokwon0429@users.noreply.github.com>
2025-05-21 22:51:20 +0900

    Update Player.h

--- CS230/Game/Player.h
+++ CS230/Game/Player.h
@@ -8,10 +8,10 @@ public:
     float maxFuel = 100.0f;
     int money = 0;
     float humanity = 0.0f;
-    float fuelBurnRate = 10.0f; // 초당 연료 소모량 (기본값)
+    float fuelBurnRate = 10.0f; 
     float expresswayBurnRate = 0.3f; 
 
-    void Update(float speed, float deltaTime); // 연료 감소 처리
-    void DrawFuelBar(Vector2 screenPosition);  // 연료 게이지 시각화
-   // void SetBurnRate(bool onExpressway); // 고속도로에서 연료 감소처리
+    void Update(float speed, float deltaTime); 
+    void DrawFuelBar(Vector2 screenPosition);  
+   // void SetBurnRate(bool onExpressway); 
 };
